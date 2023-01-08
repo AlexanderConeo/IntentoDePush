179f68f6fde420dcc59670856443720206ea48a2
Alexander Coneo <econeoj@gmail.com>
2023-01-08 16:17:56 -0500

    primer intento de commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+        System.out.println( "Hello World!" );
+    }
+
+
+}
